1e7f76c3653cd03b7b8da7a8fd2bbf6294e87e4a
Chris Nixon <chris.d.nixon@gmail.com>
2019-01-07 16:03:35 -0800

    Fix actually fetching incidents from statuspageio API. Handle the JSON response properly (success and error)

--- lib/statuspageio/client.rb
+++ lib/statuspageio/client.rb
@@ -1,11 +1,13 @@
+require 'httparty'
 require 'statuspageio/configuration'
-require 'statuspageio/request'
+require 'statuspageio/response_error'
 
 module Statuspageio
   class Client
-    require 'statuspageio/client/incident'
+    include HTTParty
+    base_uri 'api.statuspage.io/v1'
 
-    include Statuspageio::Request
+    require 'statuspageio/client/incident'
     include Statuspageio::Client::Incident
 
     attr_accessor(*Configuration::VALID_OPTIONS_KEYS)
@@ -15,6 +17,43 @@ module Statuspageio
       Configuration::VALID_OPTIONS_KEYS.each do |key|
         send("#{key}=", options[key])
       end
+    end
+
+    def self.handle_response(response)
+      if response.success?
+        JSON.parse(response.body)
+      else
+        bad_response(response)
+      end
+    end
+
+    def self.bad_response(response)
+      if response.class == HTTParty::Response
+        raise ResponseError, response
+      end
+      raise StandardError, "Unknown error"
+    end
+
+    def delete(path)
+      self.class.handle_response(self.class.delete("#{path}.json", headers: headers))
+    end
+
+    def get(path)
+      self.class.handle_response(self.class.get("#{path}.json", headers: headers))
+    end
+
+    def post(path, data = {})
+      self.class.handle_response(self.class.post("#{path}.json", data, headers: headers))
+    end
+
+    def put(path, data = {})
+      self.class.handle_response(self.class.put("#{path}.json", data, headers: headers))
+    end
+
+    private
+
+    def headers
+      { 'Authorization' => "OAuth #{self.api_key}" }
     end
   end
 end
---
+++ lib/statuspageio/response_error.rb
@@ -0,0 +1,41 @@
+module Statuspageio
+  # Error raised on a bad response
+  class ResponseError < StandardError
+
+    attr_reader :response, :code, :errors
+
+    # @param [HTTParty::Response] res
+    # @return [CloudApp::ResponseError]
+    def initialize(res)
+      @response = res.response
+      @code     = res.code
+      begin
+        @errors = parse_errors(res.parsed_response)
+      rescue JSON::ParserError
+        @errors = [res.response.body]
+      end
+    end
+
+    # Returns error code and message
+    #
+    # @return [String]
+    def to_s
+      "#{code.to_s} #{response.msg}".strip
+    end
+
+    private
+
+    def parse_errors(errors)
+      return case errors
+        when Hash
+          errors.collect{|k,v| "#{k}: #{v}"}
+        when String
+          [errors]
+        when Array
+          errors
+        else
+          []
+      end
+    end
+  end
+end
